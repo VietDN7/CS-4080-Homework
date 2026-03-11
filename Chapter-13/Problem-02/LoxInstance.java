da1d8d3a74eec6433548ed493dbe80a675c14432
Viet Nguyen <vietdn7@gmail.com>
2026-03-10 18:05:01 -0700

    Chpaters 12 & 13

---
+++ Chapter-13/Problem-02/LoxInstance.java
@@ -0,0 +1,42 @@
+//> Classes lox-instance
+package com.craftinginterpreters.lox;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class LoxInstance {
+  private LoxClass klass;
+//> lox-instance-fields
+  private final Map<String, Object> fields = new HashMap<>();
+//< lox-instance-fields
+
+  LoxInstance(LoxClass klass) {
+    this.klass = klass;
+  }
+
+//> lox-instance-get-property
+  Object get(Token name) {
+    if (fields.containsKey(name.lexeme)) {
+      return fields.get(name.lexeme);
+    }
+
+    java.util.List<LoxFunction> chain = klass.findMethodChain(name.lexeme);
+    if (!chain.isEmpty()) {
+      return new LoxMethodChain(chain, this, 0);
+    }
+
+//< lox-instance-get-method
+    throw new RuntimeError(name, // [hidden]
+        "Undefined property '" + name.lexeme + "'.");
+  }
+//< lox-instance-get-property
+//> lox-instance-set-property
+  void set(Token name, Object value) {
+    fields.put(name.lexeme, value);
+  }
+//< lox-instance-set-property
+  @Override
+  public String toString() {
+    return klass.name + " instance";
+  }
+}
